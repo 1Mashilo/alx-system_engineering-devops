e7b8a48d99f5ed589c4aede36e68c47778610e67
1Mashilo <christophermashilo@gmail.com>
2024-03-25 03:42:43 -0700

    submit task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,20 @@
+# This Puppet manifest defines a file resource for creating a file named 'school'
+# in the '/tmp' directory with specific ownership, permissions, and content.
+
+# Define a file resource
+file { '/tmp/school':
+  # Ensure the file exists
+  ensure  => present,
+
+  # Set owner to www-data user
+  owner   => 'www-data',
+
+  # Set group to www-data group
+  group   => 'www-data',
+
+  # Set file permissions (read, write, execute for owner, read for group)
+  mode    => '0744',
+
+  # Set content of the file
+  content => 'I love Puppet',
+}
